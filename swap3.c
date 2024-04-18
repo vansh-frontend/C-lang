e239404c823d686c4ade94017457db1c2d4ac69d
vansh020 <vanshdhalor04@gmail.com>
2024-04-18 20:45:09 +0530

    second

---
+++ swap3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+  int a, b;
+
+  printf(" value of a before swap: ");
+  scanf("%d", &a);
+  printf(" values of before swap b: ");
+  scanf("%d", &b);
+
+  a = a ^ b;
+  b = a ^ b;
+  a = b ^ a;
+
+  printf("\n");
+
+  printf(" value of a after swap:%d \n", a);
+  printf(" values of after swap b: %d ", b);
+
+  return 0;
+}
